fb7cee7faf69b7bda3e90cbf1d087788d9e3fb74
289Volts <289volts@gmail.com>
2022-11-12 00:52:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<div className="main-container">
+				<section className="hero-section">
+					<div className="r-side">
+						<div className="text-content text-center">
+							<h2 className="hero-heading font-bold text-[1.7rem] ">
+								Rent a <span className="text-meta">Place</span> away from <span className="text-meta">Home</span> in the{" "}
+								<span className="text-meta">Metaverse</span>
+							</h2>
+							<p className="text-base">
+								We provide you access to luxury and affordable houses in the Metaverse, get a chance to turn your
+								imagination to reality at your comfort zone.
+							</p>
+						</div>
+						<div className="input-field flex justify-center">
+							<input
+								type="text"
+								className="border-[#A3A3A3] border-[1px] p-[0.4rem] px-[0.5rem] border-solid w-[60%] rounded-tl-[4px] rounded-bl-[4px]"
+								placeholder="Search for location"
+							/>
+							<button className="bg-meta text-white py-[0.4rem] w-[35%] rounded-tr-[4px] rounded-br-[4px]">
+								Search
+							</button>
+						</div>
+					</div>
+					<div className="l-side">
+						<img src="./src/assets/group 4028.svg" alt="" className=" w-[80%] mx-auto" />
+					</div>
+				</section>
+				<section className="clients overflow-hidden  bg-meta">
+					<div className="images py-2 flex gap-[4rem]  translate-x-full">
+						<img src="./src/assets/group 59537.svg" alt="" className="w-[50%]" />
+						<img src="./src/assets/group 4040.svg" alt="" className="w-[50%]" />
+						<img src="./src/assets/frame 4041.svg" alt="" className="w-[50%]" />
+					</div>
+				</section>
+			</div>
+		</div>
+	);
+}
+
+export default App;
